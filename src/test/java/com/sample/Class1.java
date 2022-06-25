2ee96e7267e5d73037da117ce47c5678a3b79357
dhanapriya12 <shreeshan27@gmail.com>
2022-06-25 16:24:18 +0530

    Second Time Push

--- src/test/java/com/sample/Class1.java
+++ src/test/java/com/sample/Class1.java
@@ -2,4 +2,31 @@ package com.sample;
 
 public class Class1 {
 
+	
+	private void method1() {
+		System.out.println("1");
+
+	}
+	
+	private void method2() {
+		System.out.println("2");
+
+	}
+	
+	private void method3() {
+		System.out.println("3");
+	}
+	
+	private void method4() {
+		System.out.println("4");
+
+	}
+
+    private void method5() {
+		System.out.println("5");
+
+	}
+
+
 }
+
